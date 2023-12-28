b23c27645220cac1b618be14840240b70602fb3d
Eshank Tyagi <mreshanktyagi@gmail.com>
2023-12-28 17:08:39 +0530

    some em changes

--- script.js
+++ script.js
@@ -42,7 +42,35 @@ function handleColorSwitch() {
 }
 
 
-const { url } = require("inspector");
+
+
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBZPrM1iviwYerRrQrNVwwX5BLF_ZQBUII",
+    authDomain: "mreshank-github-io-weather-app.firebaseapp.com",
+    projectId: "mreshank-github-io-weather-app",
+    storageBucket: "mreshank-github-io-weather-app.appspot.com",
+    messagingSenderId: "647035684954",
+    appId: "1:647035684954:web:19e174d643970bd665f063",
+    measurementId: "G-KK1HTR9JDR"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+
+
+
+// const { url } = require("inspector");
 
 // let colorControlSwitch = 0;
 
