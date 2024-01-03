31539d46d43e59bf2bf1972fbbcd0a51ce72dee3
Eshank Tyagi <111590631+mreshank@users.noreply.github.com>
2024-01-04 02:00:37 +0530

    testing db.js

---
+++ scripts/db.js
@@ -0,0 +1,48 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+const firebaseConfig = {
+  apikey: ${{ vars.FIREBASE_API_KEY }};
+};
+
+alert("hi");
+alert( ${{ vars.FIREBASE_API_KEY }} );
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+import { getDatabase, ref, get, set, child, update, remove } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+
+const db = getDatabase();
+
+var nameInput = document.getElementById("nameInput");
+var emailInput = document.getElementById("emailInput");
+var subjectInput = document.getElementById("subjectInput");
+var messageInput = document.getElementById("messageInput");
+
+function submitContactForm(){ 
+    console.log("submitContactForm function called");
+
+    let timestamp = Date.now().toString();
+
+    set (ref(db, "contactForm/" + timestamp), {
+        Name: nameInput.value,
+        Email: emailInput.value,
+        Subject: subjectInput.value,
+        Message: messageInput.value,
+    })
+
+    .then(() =>{
+        console.log("Data stored successfully.");
+        formSubmitted();
+    })
+    .catch((error) => {
+        alert("Error in saving data" + error);
+    });
+    return false;
+}
+
+document.getElementById("contactNow").addEventListener('click', submitContactForm);
+
+function formSubmitted() {
+    console.log("Form Submitted Successfully !");
+}
