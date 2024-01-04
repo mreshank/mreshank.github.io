e998c5c31ec6585a0ce19dd61ba2d32599655594
Eshank Tyagi <mreshanktyagi@gmail.com>
2024-01-04 13:39:32 +0530

    UI changes

--- scripts/colorSwitch.js
+++ scripts/colorSwitch.js
@@ -30,9 +30,15 @@ function handleColorSwitch() {
             item.style.color = "#000000";
             item.style.backgroundColor = "#eaf0f5ce"; 
         });
-        
-        // document.querySelector(".projects").style.backgroundColor = "#eaf0f5";
-        // document.querySelector(".projects").style.boxShadow = "0 0 1vmin rgba(1, 1, 1, 0.3)";        
+
+        document.querySelectorAll(".edu-button").forEach((item) => {
+            item.style.color = "#000000";
+        });
+        document.querySelector("#edu-para").style.color = "#000000";
+        document.querySelector("#edu-para").style.backgroundImage = "linear-gradient(rgba(210, 210, 210, 0.4), rgba(150, 150, 150, 0.8))";
+        // document.querySelector("#institute-image").style
+
+            
     } else {
         colorControlSwitch = 0;
         body.style.color = "#ffffff";
@@ -46,9 +52,6 @@ function handleColorSwitch() {
         document.getElementById("WhatsApp").src = "./assets/images/whatsapp.png";
         document.getElementById("LinkTree").src = "./assets/images/linktree.png";
         document.getElementById("intro-image-and-text").style.backgroundImage = "url(./assets/images/cover.png)";
-        // navSection.nav.style.backgroundColor = "#14141470";
-        // contactInputs.style.color = "#ffffff";
-        // contactInputs.style.backgroundColor = "#181818b5";
 
         contactInputs.forEach((item) => {
             item.style.color = "#ffffff";
@@ -60,132 +63,12 @@ function handleColorSwitch() {
             item.style.backgroundColor = "#4c4c4ccc"; 
         });
 
+        document.querySelectorAll(".edu-button").forEach((item) => {
+            item.style.color = "#ffffff";
+        });
+        document.querySelector("#edu-para").style.color = "#ffffff";
+        document.querySelector("#edu-para").style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0.35))";
+        // document.querySelector
+
     }
 }
-
-
-
-
-
-
-
-
-
-
-// const { url } = require("inspector");
-
-// let colorControlSwitch = 0;
-
-// const body = document.body;
-// const navSection = document.getElementById("navigation-section");
-// const contactSection = document.getElementById("contact-section");
-
-// // Function to handle the color switch button click event
-// function handleColorSwitch() {
-//     console.log(colorControlSwitch);
-//     if (colorControlSwitch == 0) {
-//         body.style.color = "#000000";
-//         // document.body.style.backgroundColor = "#a2c0ed";
-//         body.style.backgroundColor = "#f8feff";
-//         document.getElementById("color-switch-button").style.backgroundImage = "url(./images/colorSwitch.b.png)";
-//         document.getElementById("intro-image").src = "./images/profile.c.png"
-//         document.getElementById("Instagram").src = "./images/instagram.c.png";
-//         document.getElementById("LinkedIn").src = "./images/linkedin.c.png";
-//         document.getElementById("GitHub").src = "./images/github.c.png";
-//         document.getElementById("YouTube").src = "./images/youtube.c.png";
-//         document.getElementById("WhatsApp").src = "./images/whatsapp.c.png";
-//         document.getElementById("LinkTree").src = "./images/linktree.c.png";
-//         document.getElementById("intro-image-and-text").style.backgroundImage = "url(./images/cover.bw02.png)";
-//         // document.getElementById("intro-text").style.color = "#f0f0f0";
-//         navSection.style.color = "#ffffff";
-//         navSection.nav.style.backgroundColor = "#000000";
-//         // document.getElementById("Instagram").src = "./images/instagram.c.png";
-
-//         // contactSection.div.form.div.div.input.style.backgroundColor = contactSection.div.form.input.style.backgroundColor = contactSection.div.form.div.textarea.style.backgroundColor = "#cfcfcf";
-        
-//         colorControlSwitch = 1;
-//     } else {
-//         document.body.style.color = "#ffffff";
-//         document.body.style.backgroundColor = "#1c1c1c";
-//         // document.getElementById("navigation-section").nav.backgroundColor = "#1414145e";
-//         document.getElementById("Instagram").src = "images/instagram.png";
-//         document.getElementById("Instagram").src = "instagram.png";
-//         colorControlSwitch = 0;
-//     }
-// }
-
-// // Get the color switch button element
-// const colorSwitchButton = document.getElementById('color-switch-button');
-
-// // Add event listener to the color switch button
-// colorSwitchButton.addEventListener("click", handleColorSwitch);
-
-
-
-
-
-
-
-
-
-// document.getElementById("intro-image-and-text").style.backgroundImage = "url(./images/github.png)";
-
-// import { url } from "inspector";
-// import "./styles.css";
-// const mybut = document.getElementById("color-switch-button");
-// mybut.addEventListener("click", function () {
-//     body.color = rgb(0, 0, 0);
-//     body.backgroundColor = rgb(215, 196, 255);
-//     document.getElementById("navigation-section").nav.backgroundColor = rgba(9, 0, 33, 0.621);
-//     document.getElementById("navigation-hyperlinks").a.color = rgba(211, 211, 211, 0.968);
-//     document.getElementById("intro-image").content = url("images/profile.color.png");
-//     document.getElementById("intro-image-and-text").backgroundImage = url("images/cover.color.jpg");
-// } );
-
-// function switchColorFunction() {
-//     body.color = rgb(0, 0, 0);
-//     body.backgroundColor = rgb(215, 196, 255);
-//     document.getElementById("navigation-section").nav.backgroundColor = rgba(9, 0, 33, 0.621);
-//     document.getElementById("navigation-hyperlinks").a.color = rgba(211, 211, 211, 0.968);
-//     document.getElementById("intro-image").content = url("images/profile.color.png");
-//     document.getElementById("intro-image-and-text").backgroundImage = url("images/cover.color.jpg");
-// }
-
-
-
-
-
-
-// function handleColorSwitch() {
-//         console.log(colorControlSwitch);
-//         if (colorControlSwitch == 0) {
-//             body.style.color = "#000000";
-//             // document.body.style.backgroundColor = "#a2c0ed";
-//             body.style.backgroundColor = "#f8feff";
-//             document.getElementById("color-switch-button").style.backgroundImage = "url(./images/colorSwitch.b.png)";
-//             document.getElementById("intro-image").src = "./images/profile.c.png"
-//             document.getElementById("Instagram").src = "./images/instagram.c.png";
-//             document.getElementById("LinkedIn").src = "./images/linkedin.c.png";
-//             document.getElementById("GitHub").src = "./images/github.c.png";
-//             document.getElementById("YouTube").src = "./images/youtube.c.png";
-//             document.getElementById("WhatsApp").src = "./images/whatsapp.c.png";
-//             document.getElementById("LinkTree").src = "./images/linktree.c.png";
-//             document.getElementById("intro-image-and-text").style.backgroundImage = "url(./images/cover.bw02.png)";
-//             // document.getElementById("intro-text").style.color = "#f0f0f0";
-//             navSection.style.color = "#ffffff";
-//             navSection.nav.style.backgroundColor = "#000000";
-//             // document.getElementById("Instagram").src = "./images/instagram.c.png";
-    
-//             // contactSection.div.form.div.div.input.style.backgroundColor = contactSection.div.form.input.style.backgroundColor = contactSection.div.form.div.textarea.style.backgroundColor = "#cfcfcf";
-            
-//             colorControlSwitch = 1;
-//         } else {
-//             document.body.style.color = "#ffffff";
-//             document.body.style.backgroundColor = "#1c1c1c";
-//             // document.getElementById("navigation-section").nav.backgroundColor = "#1414145e";
-//             document.getElementById("Instagram").src = "images/instagram.png";
-//             document.getElementById("Instagram").src = "instagram.png";
-//             colorControlSwitch = 0;
-//         }
-//     }
-    
